8372eb18eaebce024cd263d60397da1c0ed112f6
eloy <eloy.colell@gmail.com>
2015-12-17 13:36:20 -0300

    Fix a bug with the select_proto.

--- pfamserver/autoupdate.py
+++ pfamserver/autoupdate.py
@@ -42,6 +42,7 @@ def get_max_version(protocol):
 
 
 def get_available(config):
+    global select_proto
     available = None
     print("->\tPFam: Get availables versions from {:s}".format(server['url']))
     for p in proto:
