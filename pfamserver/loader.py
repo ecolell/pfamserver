04185ad2f71a54375e29731cebce98256ab5383a
eloy <eloy.colell@gmail.com>
2016-01-30 16:38:45 -0300

    It append the complete list of tables to deploy.

--- pfamserver/loader.py
+++ pfamserver/loader.py
@@ -53,6 +53,18 @@ def load_txt(table_name):
 
 
 def init_db():
-    tables = ['version', 'pfamA', 'pfamseq', 'uniprot']
+    tables =  ['version', 'pfamA', 'pfamseq', 'uniprot']
+    tables += ['pfamA_reg_seed', 'uniprot_reg_full', 'pfamA_reg_full_significant', 'pfamA_reg_full_insignificant']
+    tables += ['pfam_annseq', 'secondary_pfamseq_acc', 'evidence']
+    tables += ['markup_key', 'pfamseq_disulphide', 'other_reg', 'pfamseq_markup']
+    tables += ['architecture' 'pfamA_architecture']
+    tables += ['literature_reference', 'gene_ontology', 'pfamA_database_links', 'interpro', 'pfamA_literature_reference', 'pfamA_interactions']
+    tables += ['clan_alignment_and_relationship', 'clan', 'clan_database_links', 'clan_membership', 'clan_lit_ref', 'clan_architecture']
+    tables += ['dead_family', 'dead_clan']
+    tables += ['nested_locations']
+    tables += ['pdb', 'pdb_image', 'pdb_residue_data', 'pdb_pfamA_reg']
+    tables += ['proteome_regions', 'complete_proteomes', 'taxonomy', 'ncbi_taxonomy']
+    tables += ['pfamA2pfamA_scoop', 'pfamA2pfamA_hhsearch']
+    tables += ['pfamA_HMM', 'alignment_and_tree']
     map(load_sql, tables)
     map(load_txt, tables)
