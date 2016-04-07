a167d991f8a518ac5f2399b9cd2c1cdc3edca701
eloy <eloy.colell@gmail.com>
2016-04-07 15:07:56 -0300

    Use the DEBUG and DB enviromental variables to force the configuration from the console.

--- pfamserver/config.py
+++ pfamserver/config.py
@@ -1,14 +1,16 @@
 from distutils.sysconfig import get_python_lib
+from os import getenv
 
 
 config = {}
-config['DEBUG'] = True
+config['DEBUG'] = getenv('DEBUG', True)
 config['SECRET_KEY'] = 'some_secret_key!'
 config['LIB_PATH'] = get_python_lib()
 config['ROOT_PATH'] = "{:s}/pfam_data/".format(config['LIB_PATH'])
 config.update(
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@192.168.101.100:3306/pfamserver',
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/pfamserver',
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{:}'.format(
+        getenv('DB', 'root:root@localhost:3306/pfamserver')),
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db',
     #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/pfamserver',
     SESSION_TYPE = 'sqlalchemy',
