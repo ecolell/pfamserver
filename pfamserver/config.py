6ff927fc945d5288d2301bf87511b8873f8b680d
franco <francolsimonetti@gmail.com>
2017-11-03 18:17:34 +0100

    added support for local database installation

--- pfamserver/config.py
+++ pfamserver/config.py
@@ -6,11 +6,14 @@ config = {}
 config['DEBUG'] = eval(getenv('DEBUG', 'True'))
 config['SECRET_KEY'] = 'some_secret_key!'
 config['LIB_PATH'] = get_python_lib()
-config['ROOT_PATH'] = "{:s}/pfam_data/".format(config['LIB_PATH'])
+
+# path to store and look for pfam data (+100GB)
+config['ROOT_PATH'] = '/media/disk2/pfam_data/' # it is important to put the slash at the end..
+config['DBMANUAL_PATH'] = '/media/disk2/database_files'
 config.update(
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@192.168.101.100:3306/pfamserver',
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{:}'.format(
-        getenv('DB', 'root:root@localhost:3306/pfamserver')),
+        getenv('DB', 'root:fsimonetti@localhost:3306/pfamserver')),
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db',
     #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/pfamserver',
     SESSION_TYPE = 'sqlalchemy',
