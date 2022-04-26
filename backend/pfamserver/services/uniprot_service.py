4e27661f05b4640232e20cd95cde9b115d967c17
eloy <eloy.colell@gmail.com>
2022-04-25 22:43:38 -0300

    [#26] Optimize uniprots description to be a bit faster.

--- backend/pfamserver/services/uniprot_service.py
+++ backend/pfamserver/services/uniprot_service.py
@@ -20,24 +20,33 @@ def handle_no_result_found(e):
     raise UniprotServiceError("Uniprot doesn" "t exist.")
 
 
-def query_uniprot(uniprot: str):
-    uniprot = uniprot.upper()
+def query_uniprot_and_pfams_included(query_string: str):
+    uniprot = query_string.upper()
     query = db.session.query(Uniprot)
     query = query.join(Uniprot.pfams)
+    query = uniprot_query_filter(uniprot, query)
+    return query
+
+
+def uniprot_query_filter(uniprot, query):
     query = query.filter(
         or_(Uniprot.uniprot_id == uniprot, Uniprot.uniprot_acc == uniprot)
     )
+
     return query
 
 
 @merry._try
-def get_uniprot(uniprot: str):
-    return query_uniprot(uniprot).one()
+def get_uniprot(query_string: str):
+    uniprot = query_string.upper()
+    query = db.session.query(Uniprot)
+    query = uniprot_query_filter(uniprot, query)
+    return query.one()
 
 
 @merry._try
 def get_pfams_from_uniprot(uniprot):
-    query = query_uniprot(uniprot)
+    query = query_uniprot_and_pfams_included(uniprot)
     query = query.filter(UniprotRegFull.uniprot_acc == Uniprot.uniprot_acc)
     query = query.filter(PfamA.pfamA_acc == UniprotRegFull.pfamA_acc)
     query = query.order_by(UniprotRegFull.seq_start)
