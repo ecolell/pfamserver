184494b528ca761f56a7d45eb7c7b525c828bbe0
eloy <eloy.colell@gmail.com>
2022-05-02 11:41:02 -0300

    [#26] set fixture to fix a test.

--- backend/tests/api/v0/test_uniprot.py
+++ backend/tests/api/v0/test_uniprot.py
@@ -1,43 +1,35 @@
-from __future__ import unicode_literals
 import json
 
 
-def test_get_uniprot_pfams(db, client):
-    headers = [('Accept', 'application/json'),
-               ('Content-Type', 'application/json')]
-    res = client.get('/api/v0/uniprots/egfr_human/pfams', headers=headers)
+def test_get_uniprot_pfams(db, client, uniprot_reg_full_egfr_human):
+    headers = [("Accept", "application/json"), ("Content-Type", "application/json")]
+    res = client.get("/api/v0/uniprots/egfr_human/pfams", headers=headers)
     assert res.status_code == 200
     data = json.loads(res.get_data(as_text=True))
-    assert data['query'] == 'EGFR_HUMAN'
-    assert len(data['output']) == 6
+    assert data["query"] == "EGFR_HUMAN"
+    assert len(data["output"]) == 6
 
-    res = client.get('/api/v0/uniprots/invalid_uniprot/pfams', headers=headers)
+    res = client.get("/api/v0/uniprots/invalid_uniprot/pfams", headers=headers)
     assert res.status_code == 400
     data = json.loads(res.get_data(as_text=True))
-    assert data['message'] == 'Uniprot doesn''t exist.'
+    assert data["message"] == "Uniprot doesn" "t exist."
 
 
 def test_get_reference_sequences_pdbs(db, client):
-    headers = [('Accept', 'application/json'),
-               ('Content-Type', 'application/json')]
-    res = client.get('/api/v0/uniprots/MT2_HUMAN/1-61/pdbs', headers=headers)
+    headers = [("Accept", "application/json"), ("Content-Type", "application/json")]
+    res = client.get("/api/v0/uniprots/MT2_HUMAN/1-61/pdbs", headers=headers)
     assert res.status_code == 200
     data = json.loads(res.get_data(as_text=True))
-    assert data['query'] == {
-        'seq_end': 61,
-        'seq_start': 1,
-        'uniprot_id': 'MT2_HUMAN'
-    }
-    assert len(data['output']) == 2
+    assert data["query"] == {"seq_end": 61, "seq_start": 1, "uniprot_id": "MT2_HUMAN"}
+    assert len(data["output"]) == 2
 
 
 def test_get_uniprots(db, client):
-    headers = [('Accept', 'application/json'),
-               ('Content-Type', 'application/json')]
+    headers = [("Accept", "application/json"), ("Content-Type", "application/json")]
 
-    res = client.get('/api/v0/uniprots/egfr_human', headers=headers)
+    res = client.get("/api/v0/uniprots/egfr_human", headers=headers)
     assert res.status_code == 200
     data = json.loads(res.get_data(as_text=True))
-    assert data['description'] == 'Epidermal growth factor receptor EC=2.7.10.1'
-    assert data['uniprot_id'] == 'EGFR_HUMAN'
-    assert data['uniprot_acc'] == 'P00533'
+    assert data["description"] == "Epidermal growth factor receptor EC=2.7.10.1"
+    assert data["uniprot_id"] == "EGFR_HUMAN"
+    assert data["uniprot_acc"] == "P00533"
