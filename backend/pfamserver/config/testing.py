a5ba3059803deec7bec809028501dfceedafca60
eloy <eloy.colell@gmail.com>
2022-05-27 10:31:07 -0300

    [#26] Remove difference between testing and base config.

--- backend/pfamserver/config/testing.py
+++ backend/pfamserver/config/testing.py
@@ -1,14 +1,9 @@
-import os
-
 from pfamserver.config.base import BaseConfig
 
 
 class TestingConfig(BaseConfig):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:"
-    )
 
     @staticmethod
     def init_app(app):
