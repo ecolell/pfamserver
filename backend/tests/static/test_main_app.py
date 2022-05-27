3ce20bd68420398a3e35040bc19304b6bc52b48c
eloy <eloy.colell@gmail.com>
2022-05-27 10:36:30 -0300

    [#26] Cleanup test_main_app test. db not require import on api. client already setup the app.

--- backend/tests/static/test_main_app.py
+++ backend/tests/static/test_main_app.py
@@ -1,9 +1,6 @@
-import re
-
-
-def test_main_app(db, client, main_app_template):
-    res = client.get('/')
+def test_main_app(client, main_app_template):
+    res = client.get("/")
     assert res.status_code == 200
-    data = res.get_data(as_text=True).split('\n')
-    diffs = [l for l in zip(main_app_template, data) if l[0] != l[1]]
+    data = res.get_data(as_text=True).split("\n")
+    diffs = [line for line in zip(main_app_template, data) if line[0] != line[1]]
     assert len(diffs) <= 4
