6e050cfa8f0073d81db68e9c439413e826ba1c0e
eloy <eloy.colell@gmail.com>
2022-06-01 11:02:17 -0300

    [#26] Improve test_version.

--- backend/tests/services/test_version_service.py
+++ backend/tests/services/test_version_service.py
@@ -1,8 +1,8 @@
-from __future__ import unicode_literals
 from pfamserver.services import version_service as service
 
 
 def test_version(app, current_version):
     with app.app_context():
-        assert app.config.get('SQLALCHEMY_DATABASE_URI')[-8:] == current_version.replace('.', '_')
+        db_name = app.config.get("SQLALCHEMY_DATABASE_URI").split("/")[-1]
+        assert db_name[:8] == current_version.replace(".", "_")
         assert service.version() == current_version
