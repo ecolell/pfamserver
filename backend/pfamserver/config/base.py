ae32227b54b1e7acd1f7aad3038fba59b138d021
eloy <eloy.colell@gmail.com>
2022-07-25 23:01:07 -0300

    [#26] Setup redis-cache properly on the flask-caching extension.

--- backend/pfamserver/config/base.py
+++ backend/pfamserver/config/base.py
@@ -26,6 +26,14 @@ class BaseConfig:
     SQLALCHEMY_ECHO = False  # True to log queries
     TABLE_CACHE_ENABLED = os.getenv("TABLE_CACHE_ENABLED", True)
 
+    # Caching
+    CACHE_TYPE = os.environ.get("CACHE_TYPE", "RedisCache")
+    CACHE_REDIS_HOST = os.environ.get("CACHE_REDIS_HOST", "redis_cache")
+    CACHE_REDIS_PORT = os.environ.get("CACHE_REDIS_PORT", 6379)
+    CACHE_REDIS_DB = os.environ.get("CACHE_REDIS_DB", 0)
+    CACHE_REDIS_URL = os.environ.get("CACHE_REDIS_URL", "redis://redis_cache:6379/0")
+    CACHE_DEFAULT_TIMEOUT = os.environ.get("CACHE_DEFAULT_TIMEOUT", 500)
+
     # Pagination setting
     ITEMS_PER_PAGE = 10
 
