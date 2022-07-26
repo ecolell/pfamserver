db44f07652970ad80d498b1f3fff0bc5063c24d3
eloy <eloy.colell@gmail.com>
2022-07-25 23:08:48 -0300

    [#26] Setup redis-cache properly on the flask-caching extension.

--- backend/pfamserver/extensions.py
+++ backend/pfamserver/extensions.py
@@ -4,6 +4,6 @@ from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
-cache = Cache(config={"CACHE_TYPE": "simple"})
+cache = Cache()
 csrf = CSRFProtect()
 db = SQLAlchemy()  # session_options={"autoflush": False})
