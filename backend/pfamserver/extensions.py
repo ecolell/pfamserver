f1b93779783f462fb270393e6614b4a00f9c91e8
eloy <eloy.colell@gmail.com>
2022-09-06 19:05:34 -0300

    [#34] Refactor caching behaviour from api endpoints.

--- backend/pfamserver/extensions.py
+++ backend/pfamserver/extensions.py
@@ -3,7 +3,15 @@
 from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask import request
+
 
 cache = Cache()
 csrf = CSRFProtect()
 db = SQLAlchemy()  # session_options={"autoflush": False})
+
+
+def make_cache_key(*args, **kwargs):
+    path = request.path
+    args = str(list(request.args.items()))
+    return path + args
