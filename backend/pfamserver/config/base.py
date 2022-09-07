33c0661f9910c60c42921c2ffba566c1b1bd1c0f
eloy <eloy.colell@gmail.com>
2022-09-07 18:07:36 -0300

    [#34] Fix bug for flask caching.

--- backend/pfamserver/config/base.py
+++ backend/pfamserver/config/base.py
@@ -24,10 +24,10 @@ class BaseConfig:
 
     # Caching
     CACHE_TYPE = os.environ.get("CACHE_TYPE", "RedisCache")
-    CACHE_REDIS_HOST = os.environ.get("CACHE_REDIS_HOST", "redis_cache")
+    CACHE_REDIS_HOST = os.environ.get("CACHE_REDIS_HOST", "redis-cache")
     CACHE_REDIS_PORT = os.environ.get("CACHE_REDIS_PORT", 6379)
     CACHE_REDIS_DB = os.environ.get("CACHE_REDIS_DB", 0)
-    CACHE_REDIS_URL = os.environ.get("CACHE_REDIS_URL", "redis://redis_cache:6379/0")
+    CACHE_REDIS_URL = os.environ.get("CACHE_REDIS_URL", "redis://redis-cache:6379/0")
     CACHE_DEFAULT_TIMEOUT = os.environ.get("CACHE_DEFAULT_TIMEOUT", 500)
 
     # Pagination setting
